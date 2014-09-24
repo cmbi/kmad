30056dd7c7d5ad3e09a6c4e52bfeb4f1168e63a1
Jon Black <Jon.Black@radboudumc.nl>
2014-09-24 10:20:10 +0200

    Add new substitution matrix

---
+++ substitutionMatrix.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+class Residue;
+namespace substitutionMatrix{
+	void printSbstMatrix();
+	std::vector< std::vector<double> > convertToProfileFormat(std::vector<Residue>);
+	//getters
+	char getLetter(int);
+	int getElement(char,char);
+	int getElement(int,int);
+	std::vector<int> getColumn(int);
+	int findAminoAcidsNo(char);
+};
