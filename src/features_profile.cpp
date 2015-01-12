d3cbd0c1c5fa114c78ef128882628a2570ff8900
Joanna Lange <joasialange@gmail.com>
2015-01-12 11:04:06 +0100

    Impementation of FeaturesProfile struct

--- src/features_profile.cpp
+++ src/features_profile.cpp
@@ -10,6 +10,154 @@
 #include <vector>
 #include <tuple>
 
+
+FeaturesProfile::FeaturesProfile(std::vector<std::string> features,
+    int domain_modifier, int ptm_modifier, int motif_modifier,
+    std::map<std::string, double> motif_probabilities)
+: m_domain_modifier(domain_modifier),
+  m_ptm_modifier(ptm_modifier),
+  m_motif_modifier(motif_modifier),
+  m_features(features),
+  m_motif_probabilities(motif_probabilities)
+{}
+
+
+void FeaturesProfile::create_score_features_profile(
+    const fasta::SequenceList& sequences) {
+  m_occurences = create_features_profile(sequences);
+  // convert occurences to probabilities
+  for (auto& occ: m_occurences) {
+    size_t i = 0;
+    for (auto& v: occ.second) {
+      occ.second[i] = v / sequences.size();
+      i++;
+    }
+  }
+  // create an empty score map 
+  FeaturesProfileMap scores;
+  for (auto& f: m_features) {
+    scores[f] = std::vector<double>(sequences[0].residues.size(), 0);
+  }
+  for (size_t i = 0; i < sequences[0].residues.size(); i++) {
+    for (auto &feat : m_features){
+      if (feat.substr(0,3) == "ptm") {
+        scores[feat][i] = score_ptm(i, feat);
+      } else if (feat.substr(0,6) == "domain") {
+        scores[feat][i] = score_domain(i, feat);
+      } else if (feat.substr(0,5) == "motif") {
+        scores[feat][i] = score_motif(i, feat);
+      } 
+      // else if (feat.substr(0,3) == "USR") {
+      //   score_p[feat][i] = score_usr_feature(p, i, feat);
+      // }
+    }
+  }
+  m_scores = scores;
+}
+
+
+FeaturesProfileMap FeaturesProfile::create_features_profile(
+    const fasta::SequenceList& sequences) {
+  FeaturesProfileMap p;
+  for (auto& f: m_features) {
+    p[f] = std::vector<double>(sequences[0].residues.size(), 0);
+  }
+  for (size_t i = 0; i < sequences[0].residues.size(); i++) {
+    for (size_t j = 0; j < sequences.size(); j++) {
+      for (auto& f : sequences[j].residues[i].features) {
+        assert(p.find(f) != p.end());
+        p[f][i] += 1.0;
+      }
+    }
+  }
+  return p;
+}
+
+
+double FeaturesProfile::score_ptm(unsigned long position, 
+                                  std::string ptm_name) {
+  double result  = 0;
+  std::string ptm_type = ptm_name;
+  //pop back last character to get just the ptm type
+  ptm_type.pop_back();
+  // level of annotation - last character of feature's name
+  char ptm_level = ptm_name.back();
+  double ptm_score = 0.;
+  // first set ptm_score based on annotation level of the query ptm
+  if (ptm_level == '0') {
+    ptm_score = 1.0;
+  } else if (ptm_level == '1') {
+    ptm_score = 0.9;
+  } else if (ptm_level == '2') {
+    ptm_score = 0.8;
+  } else if (ptm_level == '3') {
+    ptm_score = 0.7;
+  } else if (ptm_level == 'P') {
+    ptm_score = 0.3;
+  } else {
+    std::string msg = "wrong annotation level on position "
+                    + std::to_string(position)
+                    + " ptmname: " + ptm_name;
+    throw std::invalid_argument(msg);
+  }
+  for (auto feat_it = m_occurences.begin();
+       feat_it != m_occurences.end(); feat_it++) {
+    std::string i_name = feat_it->first;
+    std::string i_type = i_name;
+    // popping back last character, to get just the ptm type
+    // (without its level of annotation)
+    i_type.pop_back();
+    if (i_type == ptm_type) {
+      char i_level = i_name.back();
+      if (i_level == '0') {
+        result += feat_it->second[position];
+      } else if (i_level == '1') {
+        result += feat_it->second[position] * 0.9;
+      } else if (i_level == '2') {
+        result += feat_it->second[position] * 0.8;
+      } else if (i_level == '3') {
+        result += feat_it->second[position] * 0.7;
+      } else if (i_level == 'P') {
+        result += feat_it->second[position] * 0.3;
+      }
+    }
+  }
+  result = result * ptm_score * m_ptm_modifier;
+  return result;
+}
+
+
+double FeaturesProfile::score_domain(unsigned long position, 
+                                     std::string dom_name) {
+  double result = 0;
+  for (auto feat_it = m_occurences.begin(); 
+       feat_it != m_occurences.end(); feat_it++) {
+    if (feat_it->first == dom_name) {
+      result += feat_it->second[position];
+    } else if (feat_it->first.substr(0,6) == "domain") {
+      result -= feat_it->second[position];
+    }
+  }
+  return result * m_domain_modifier;
+}
+
+
+double FeaturesProfile::score_motif(unsigned long position,
+                                    std::string feat_name) {
+  double result = 0;
+  bool not_found = true;
+  for (auto feat_it = m_occurences.begin();
+       feat_it != m_occurences.end() && not_found; feat_it++) {
+    if (feat_it->first == feat_name) {
+      result = feat_it->second[position] * m_motif_modifier \
+               * m_motif_probabilities[feat_name];
+      not_found = false;     
+    }
+  }
+  return result;
+}
+
+
 FeaturesProfileMap create_score_features_profile(
     const fasta::SequenceList& sequences, 
     const std::vector<std::string>& features, int ptm_modifier, 
--- src/features_profile.h
+++ src/features_profile.h
@@ -16,16 +16,18 @@ struct FeaturesProfile {
   int m_domain_modifier;
   int m_ptm_modifier;
   int m_motif_modifier;
+  std::vector<std::string> m_features;
   std::map<std::string, double> m_motif_probabilities;
-  FeaturesProfileMap m_feature_profile_scores;
+  FeaturesProfileMap m_scores;
+  FeaturesProfileMap m_occurences;
 
-  FeaturesProfile(const std::vector<std::string>& features,
+
+  FeaturesProfile(const std::vector<std::string> features,
                   int domain_modifier, int ptm_modifier, int motif_modifier,
                   std::map<std::string, double> motif_probabilities);
   FeaturesProfileMap create_features_profile(
       const fasta::SequenceList& sequences);
-  FeaturesProfileMap create_score_features_profile(
-      const fasta::SequenceList& sequences);
+  void create_score_features_profile(const fasta::SequenceList& sequences);
   double score_ptm(unsigned long position, std::string feat_name);
   double score_domain(unsigned long position, std::string feat_name);
   double score_motif(unsigned long position, std::string feat_name);
