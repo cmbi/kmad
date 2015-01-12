6f8314cb08d69351c330259a75b756887ba361e9
Joanna Lange <joasialange@gmail.com>
2015-01-12 10:24:26 +0100

    FeaturesProfile struct header

--- src/features_profile.cpp
+++ src/features_profile.cpp
@@ -10,7 +10,6 @@
 #include <vector>
 #include <tuple>
 
-
 FeaturesProfileMap create_score_features_profile(
     const fasta::SequenceList& sequences, 
     const std::vector<std::string>& features, int ptm_modifier, 
@@ -92,173 +91,173 @@ namespace {
 }
 
 
-FeaturesProfile::FeaturesProfile(int dom, int phosph, int motif,
-                                 std::map<std::string, double> probs)
-: m_domain_score(dom),
-  m_phosph_score(phosph),
-  m_motif_score(motif)
-{
-  // TODO: Use probabilities directly
-  for (const auto& kv: probs) {
-    m_motifs_ids.push_back(kv.first);
-    m_motifs_probs.push_back(kv.second);
-  }
-}
-
-
-void FeaturesProfile::CreateProfile(const std::vector<fasta::Sequence>& alignment,
-                                    int codon_length) {
-  CountOccurences(alignment, codon_length);
-  ProcessProfile();
-}
-
-
-void FeaturesProfile::ProcessProfile() {
-  m_prf_matrix.clear();
-  for (unsigned int i = 0; i < m_occurences_matrix.size(); i++) {
-    std::string feat_name = list_of_features[i];
-    ProfileMatrixRow feature_row(m_occurences_matrix[i].size());
-    for (unsigned int j = 0; j < m_occurences_matrix[i].size(); j++) {
-      if (feat_name.substr(0,3) == "ptm") {
-        feature_row[j] = ScorePTMs(j, feat_name);
-      } else if (feat_name.substr(0,6) == "domain") {
-        feature_row[j] = ScoreDomains(j, feat_name);
-      } else if (feat_name.substr(0,5) == "motif") {
-        feature_row[j] = ScoreMotifs(j, feat_name);
-      } else if (feat_name.substr(0,3) == "USR") {
-        feature_row[j] = ScoreUsrFeatures(j, feat_name);
-      }
-    }
-    m_prf_matrix.push_back(feature_row);
-  }
-}
-
-
-// TODO: Implement
-void FeaturesProfile::CountOccurences(const std::vector<fasta::Sequence>& alignment,
-                                      int codon_length) {
-  //m_occurences_matrix.clear();
-  //std::string nothing = "AA";
-  //int no_of_sequences = alignment.size();
-
-  //for (unsigned int i = 0; i < alignment[0].size(); i++) {
-    //ProfileMatrixColumn profile_column(list_of_features.size(),0);
-    //int non_gaps = 0;
-    //for (unsigned int j = 0; j < alignment.size(); j++) {
-      //if (alignment[j][i].get_aa() != '-') {
-        //FeatureNamesList features = alignment[j][i].get_features();
-        //for (unsigned int k = 0; k < features.size(); k++) {
-          //std::string feat_name = features[k];
-          //if (feat_name != nothing) {
-            //int feat_index = FindFeaturesIndex(feat_name);
-            //profile_column[feat_index] += GetModifier(feat_name);
-          //}
-        //}
-      //non_gaps++;
-      //}
-    //}
-    //vec_util::DivideVectorByAScalar(profile_column, no_of_sequences);
-    ////vec_util::DivideVectorByAScalar(profileColumn,nonGaps);
-    //m_occurences_matrix.push_back(profile_column);
-  //}
-  //vec_util::TransposeVec(m_occurences_matrix);
-}
-
-
-double FeaturesProfile::GetMotifsProb(std::string& m_id) {
-  double prob = 0;
-
-  FeatNameSplit motif_name;
-  boost::split(motif_name, m_id, boost::is_any_of("_"));
-  std::string id_code = motif_name[1];
-
-  assert(m_motifs_ids.size() == m_motifs_probs.size());
-  for (unsigned int i = 0; i < m_motifs_ids.size(); i++) {
-    if (m_motifs_ids[i] == id_code) {
-      prob = m_motifs_probs[i];
-      break;
-    }
-  }
-  return prob;
-}
-
-
-void FeaturesProfile::get_score(unsigned int position, FeaturesList& features,
-                                double& add_score) {
-  for (auto &feat : features) {
-       add_score += m_prf_matrix[feat][position];
-  }
-}
-
-
-double FeaturesProfile::ScoreMotifs(unsigned int& position,
-                                    std::string& feat_name) {
-  int features_index = FindFeaturesIndex(feat_name);
-  double result = 0;
-  if (features_index == -1) {
-    result = 0;
-  } else {
-    result = m_occurences_matrix[features_index][position];
-  }
-  return result;
-}
-
-
-int FeaturesProfile::FindFeaturesIndex(std::string& feat_name) {
-  int features_index = -1;
-  for (unsigned int i = 0; i < list_of_features.size(); i++) {
-    if (feat_name == list_of_features[i]) {
-      features_index = i;
-      break;
-    }
-  }
-  return features_index;
-}
-
-
-void FeaturesProfile::ExpandListOfFeatures(const std::vector<fasta::Sequence> sequences)
-{
-  // TODO: Implement
-  // TODO: Don't mess with stuff in the anonymous namespace.
-
-  //for (unsigned int i = 0; i < sequences.size(); i++) {
-    //for (unsigned int j = 0; j < sequences[i].size(); j++) {
-      //FeatureNamesList features = sequences[i][j].get_features();
-      //for (unsigned int k = 0; k < features.size(); k++) {
-        //std::string feature_k = features[k];
-        ////check whether this domain is already in the list of features
-        //if ((std::find(list_of_features.begin(), list_of_features.end(),
-                      //feature_k) == list_of_features.end())
-            //&& feature_k != nothing) {
-          //list_of_features.push_back(feature_k);
-          //if (feature_k.substr(0, 6) == "domain") {
-            ////to look later for domains only in these positions (saves time)
-            //domain_indexes.push_back(list_of_features.size() - 1);
-          //} else if (feature_k.substr(0, 5) == "motif") {
-            //// same as with domain_indexes
-            //motif_indexes.push_back(list_of_features.size()-1);
-          //}
-        //}
-      //}
-    //}
-  //}
-}
-
-
-double FeaturesProfile::ScoreDomains(unsigned int& position,
-                                     std::string& dom_name) {
-  double result = 0;
-  for (unsigned int i = 0; i < domain_indexes.size(); i++) {
-    int dom_index = domain_indexes[i];
-    if (list_of_features[dom_index] == dom_name) {
-      result += m_occurences_matrix[dom_index][position];
-    } else {
-      // subtract score for every other domain
-      result -= m_occurences_matrix[dom_index][position];
-    }
-  }
-  return result;
-}
+// FeaturesProfile::FeaturesProfile(int dom, int phosph, int motif,
+//                                  std::map<std::string, double> probs)
+// : m_domain_score(dom),
+//   m_phosph_score(phosph),
+//   m_motif_score(motif)
+// {
+//   // TODO: Use probabilities directly
+//   for (const auto& kv: probs) {
+//     m_motifs_ids.push_back(kv.first);
+//     m_motifs_probs.push_back(kv.second);
+//   }
+// }
+// 
+// 
+// void FeaturesProfile::CreateProfile(const std::vector<fasta::Sequence>& alignment,
+//                                     int codon_length) {
+//   CountOccurences(alignment, codon_length);
+//   ProcessProfile();
+// }
+// 
+// 
+// void FeaturesProfile::ProcessProfile() {
+//   m_prf_matrix.clear();
+//   for (unsigned int i = 0; i < m_occurences_matrix.size(); i++) {
+//     std::string feat_name = list_of_features[i];
+//     ProfileMatrixRow feature_row(m_occurences_matrix[i].size());
+//     for (unsigned int j = 0; j < m_occurences_matrix[i].size(); j++) {
+//       if (feat_name.substr(0,3) == "ptm") {
+//         feature_row[j] = ScorePTMs(j, feat_name);
+//       } else if (feat_name.substr(0,6) == "domain") {
+//         feature_row[j] = ScoreDomains(j, feat_name);
+//       } else if (feat_name.substr(0,5) == "motif") {
+//         feature_row[j] = ScoreMotifs(j, feat_name);
+//       } else if (feat_name.substr(0,3) == "USR") {
+//         feature_row[j] = ScoreUsrFeatures(j, feat_name);
+//       }
+//     }
+//     m_prf_matrix.push_back(feature_row);
+//   }
+// }
+// 
+// 
+// // TODO: Implement
+// void FeaturesProfile::CountOccurences(const std::vector<fasta::Sequence>& alignment,
+//                                       int codon_length) {
+//   //m_occurences_matrix.clear();
+//   //std::string nothing = "AA";
+//   //int no_of_sequences = alignment.size();
+// 
+//   //for (unsigned int i = 0; i < alignment[0].size(); i++) {
+//     //ProfileMatrixColumn profile_column(list_of_features.size(),0);
+//     //int non_gaps = 0;
+//     //for (unsigned int j = 0; j < alignment.size(); j++) {
+//       //if (alignment[j][i].get_aa() != '-') {
+//         //FeatureNamesList features = alignment[j][i].get_features();
+//         //for (unsigned int k = 0; k < features.size(); k++) {
+//           //std::string feat_name = features[k];
+//           //if (feat_name != nothing) {
+//             //int feat_index = FindFeaturesIndex(feat_name);
+//             //profile_column[feat_index] += GetModifier(feat_name);
+//           //}
+//         //}
+//       //non_gaps++;
+//       //}
+//     //}
+//     //vec_util::DivideVectorByAScalar(profile_column, no_of_sequences);
+//     ////vec_util::DivideVectorByAScalar(profileColumn,nonGaps);
+//     //m_occurences_matrix.push_back(profile_column);
+//   //}
+//   //vec_util::TransposeVec(m_occurences_matrix);
+// }
+// 
+// 
+// double FeaturesProfile::GetMotifsProb(std::string& m_id) {
+//   double prob = 0;
+// 
+//   FeatNameSplit motif_name;
+//   boost::split(motif_name, m_id, boost::is_any_of("_"));
+//   std::string id_code = motif_name[1];
+// 
+//   assert(m_motifs_ids.size() == m_motifs_probs.size());
+//   for (unsigned int i = 0; i < m_motifs_ids.size(); i++) {
+//     if (m_motifs_ids[i] == id_code) {
+//       prob = m_motifs_probs[i];
+//       break;
+//     }
+//   }
+//   return prob;
+// }
+// 
+// 
+// void FeaturesProfile::get_score(unsigned int position, FeaturesList& features,
+//                                 double& add_score) {
+//   for (auto &feat : features) {
+//        add_score += m_prf_matrix[feat][position];
+//   }
+// }
+// 
+// 
+// double FeaturesProfile::ScoreMotifs(unsigned int& position,
+//                                     std::string& feat_name) {
+//   int features_index = FindFeaturesIndex(feat_name);
+//   double result = 0;
+//   if (features_index == -1) {
+//     result = 0;
+//   } else {
+//     result = m_occurences_matrix[features_index][position];
+//   }
+//   return result;
+// }
+// 
+// 
+// int FeaturesProfile::FindFeaturesIndex(std::string& feat_name) {
+//   int features_index = -1;
+//   for (unsigned int i = 0; i < list_of_features.size(); i++) {
+//     if (feat_name == list_of_features[i]) {
+//       features_index = i;
+//       break;
+//     }
+//   }
+//   return features_index;
+// }
+// 
+// 
+// void FeaturesProfile::ExpandListOfFeatures(const std::vector<fasta::Sequence> sequences)
+// {
+//   // TODO: Implement
+//   // TODO: Don't mess with stuff in the anonymous namespace.
+// 
+//   //for (unsigned int i = 0; i < sequences.size(); i++) {
+//     //for (unsigned int j = 0; j < sequences[i].size(); j++) {
+//       //FeatureNamesList features = sequences[i][j].get_features();
+//       //for (unsigned int k = 0; k < features.size(); k++) {
+//         //std::string feature_k = features[k];
+//         ////check whether this domain is already in the list of features
+//         //if ((std::find(list_of_features.begin(), list_of_features.end(),
+//                       //feature_k) == list_of_features.end())
+//             //&& feature_k != nothing) {
+//           //list_of_features.push_back(feature_k);
+//           //if (feature_k.substr(0, 6) == "domain") {
+//             ////to look later for domains only in these positions (saves time)
+//             //domain_indexes.push_back(list_of_features.size() - 1);
+//           //} else if (feature_k.substr(0, 5) == "motif") {
+//             //// same as with domain_indexes
+//             //motif_indexes.push_back(list_of_features.size()-1);
+//           //}
+//         //}
+//       //}
+//     //}
+//   //}
+// }
+// 
+// 
+// double FeaturesProfile::ScoreDomains(unsigned int& position,
+//                                      std::string& dom_name) {
+//   double result = 0;
+//   for (unsigned int i = 0; i < domain_indexes.size(); i++) {
+//     int dom_index = domain_indexes[i];
+//     if (list_of_features[dom_index] == dom_name) {
+//       result += m_occurences_matrix[dom_index][position];
+//     } else {
+//       // subtract score for every other domain
+//       result -= m_occurences_matrix[dom_index][position];
+//     }
+//   }
+//   return result;
+// }
 
 
 double score_domain(FeaturesProfileMap& p, unsigned long position,
@@ -349,106 +348,106 @@ double score_ptm(FeaturesProfileMap& p, unsigned long position,
 }
 
 
-double FeaturesProfile::ScorePTMs(unsigned int& position,
-                                  std::string& ptm_name) {
-  double result  = 0;
-  std::string ptm_type = ptm_name;
-  //pop back last character to get just the ptm type
-  ptm_type.pop_back();
-  // level of annotation - last character of feature's name
-  char ptm_level = ptm_name.back();
-  double ptm_score;
-  // first set ptm_score based on annotation level of the query ptm
-  if (ptm_level == '0') {
-    ptm_score = 1.0;
-  } else if (ptm_level == '1') {
-    ptm_score = 0.9;
-  } else if (ptm_level == '2') {
-    ptm_score = 0.8;
-  } else if (ptm_level == '3') {
-    ptm_score = 0.7;
-  } else if (ptm_level == 'P') {
-    ptm_score = 0.3;
-  } else {
-    std::string msg = "wrong annotation level on position "
-                    + std::to_string(position)
-                    + " ptmname: " + ptm_name;
-    throw std::invalid_argument(msg);
-  }
-  // now go through list of features to find in which rows in profile
-  // are the features that we're gonna score for
-  assert(m_occurences_matrix.size() == list_of_features.size());
-  for (unsigned int i = 0; i < list_of_features.size(); i++) {
-    std::string i_name = list_of_features[i];
-    std::string i_type = i_name;
-    // popping back last character, to get just the ptm type
-    // (without its level of annotation)
-    i_type.pop_back();
-    if (i_type == ptm_type) {
-      char i_level = i_name.back();
-      if (i_level == '0') {
-        result += m_occurences_matrix[i][position];
-      } else if (i_level == '1') {
-        result += m_occurences_matrix[i][position] * 0.9;
-      } else if (i_level == '2') {
-        result += m_occurences_matrix[i][position] * 0.8;
-      } else if (i_level == '3') {
-        result += m_occurences_matrix[i][position] * 0.7;
-      } else if (i_level == 'P') {
-        result += m_occurences_matrix[i][position] * 0.3;
-      }
-    }
-  }
-  result = result * ptm_score;
-  return result;
-}
-
-
-double FeaturesProfile::ScoreUsrFeatures(unsigned int& position,
-                                         std::string& feat_name) {
-  //first find tuple(s) with rules for this feature
-  double result = 0;
-  for (auto &rule : m_rules) {
-    if (std::get<0>(rule) == feat_name) {
-      double add_tmp = std::get<2>(rule);
-      //positions of increasing features
-      FeaturesList incr_features = std::get<6>(rule);
-      //go through features that increase the score
-      for (auto &feat : incr_features) {
-        double prf_score = m_occurences_matrix[feat][position];
-        if (prf_score != 0) {
-           result += add_tmp*prf_score;
-        }
-      }
-      //the same for decreasing features
-      add_tmp = std::get<4>(rule);
-      FeaturesList decr_features = std::get<7>(rule);
-      //go through features that increase the score
-      for (auto &feat : decr_features) {
-        double prf_score = m_occurences_matrix[feat][position];
-        if (prf_score != 0) {
-          result -= add_tmp*prf_score;
-        }
-      }
-    }
-  }
-  return result;
-}
-
-
-double FeaturesProfile::GetModifier(std::string& feat_name) {
-
-  FeatNameSplit feat_name_vec;
-  boost::split(feat_name_vec, feat_name, boost::is_any_of("_"));
-  std::string feat_code  = feat_name_vec[0];
-
-  double modifier = 1;
-  if (feat_code == "ptm") {
-    modifier = m_phosph_score;
-  } else if (feat_code == "domain") {
-    modifier = m_domain_score;
-  } else if (feat_code == "motif") {
-    modifier = m_motif_score*GetMotifsProb(feat_name);
-  }
-  return modifier;
-}
+// double FeaturesProfile::ScorePTMs(unsigned int& position,
+//                                   std::string& ptm_name) {
+//   double result  = 0;
+//   std::string ptm_type = ptm_name;
+//   //pop back last character to get just the ptm type
+//   ptm_type.pop_back();
+//   // level of annotation - last character of feature's name
+//   char ptm_level = ptm_name.back();
+//   double ptm_score;
+//   // first set ptm_score based on annotation level of the query ptm
+//   if (ptm_level == '0') {
+//     ptm_score = 1.0;
+//   } else if (ptm_level == '1') {
+//     ptm_score = 0.9;
+//   } else if (ptm_level == '2') {
+//     ptm_score = 0.8;
+//   } else if (ptm_level == '3') {
+//     ptm_score = 0.7;
+//   } else if (ptm_level == 'P') {
+//     ptm_score = 0.3;
+//   } else {
+//     std::string msg = "wrong annotation level on position "
+//                     + std::to_string(position)
+//                     + " ptmname: " + ptm_name;
+//     throw std::invalid_argument(msg);
+//   }
+//   // now go through list of features to find in which rows in profile
+//   // are the features that we're gonna score for
+//   assert(m_occurences_matrix.size() == list_of_features.size());
+//   for (unsigned int i = 0; i < list_of_features.size(); i++) {
+//     std::string i_name = list_of_features[i];
+//     std::string i_type = i_name;
+//     // popping back last character, to get just the ptm type
+//     // (without its level of annotation)
+//     i_type.pop_back();
+//     if (i_type == ptm_type) {
+//       char i_level = i_name.back();
+//       if (i_level == '0') {
+//         result += m_occurences_matrix[i][position];
+//       } else if (i_level == '1') {
+//         result += m_occurences_matrix[i][position] * 0.9;
+//       } else if (i_level == '2') {
+//         result += m_occurences_matrix[i][position] * 0.8;
+//       } else if (i_level == '3') {
+//         result += m_occurences_matrix[i][position] * 0.7;
+//       } else if (i_level == 'P') {
+//         result += m_occurences_matrix[i][position] * 0.3;
+//       }
+//     }
+//   }
+//   result = result * ptm_score;
+//   return result;
+// }
+// 
+// 
+// double FeaturesProfile::ScoreUsrFeatures(unsigned int& position,
+//                                          std::string& feat_name) {
+//   //first find tuple(s) with rules for this feature
+//   double result = 0;
+//   for (auto &rule : m_rules) {
+//     if (std::get<0>(rule) == feat_name) {
+//       double add_tmp = std::get<2>(rule);
+//       //positions of increasing features
+//       FeaturesList incr_features = std::get<6>(rule);
+//       //go through features that increase the score
+//       for (auto &feat : incr_features) {
+//         double prf_score = m_occurences_matrix[feat][position];
+//         if (prf_score != 0) {
+//            result += add_tmp*prf_score;
+//         }
+//       }
+//       //the same for decreasing features
+//       add_tmp = std::get<4>(rule);
+//       FeaturesList decr_features = std::get<7>(rule);
+//       //go through features that increase the score
+//       for (auto &feat : decr_features) {
+//         double prf_score = m_occurences_matrix[feat][position];
+//         if (prf_score != 0) {
+//           result -= add_tmp*prf_score;
+//         }
+//       }
+//     }
+//   }
+//   return result;
+// }
+// 
+// 
+// double FeaturesProfile::GetModifier(std::string& feat_name) {
+// 
+//   FeatNameSplit feat_name_vec;
+//   boost::split(feat_name_vec, feat_name, boost::is_any_of("_"));
+//   std::string feat_code  = feat_name_vec[0];
+// 
+//   double modifier = 1;
+//   if (feat_code == "ptm") {
+//     modifier = m_phosph_score;
+//   } else if (feat_code == "domain") {
+//     modifier = m_domain_score;
+//   } else if (feat_code == "motif") {
+//     modifier = m_motif_score*GetMotifsProb(feat_name);
+//   }
+//   return modifier;
+// }
--- src/features_profile.h
+++ src/features_profile.h
@@ -10,8 +10,27 @@
 #include <vector>
 #include <map>
 
-
 typedef std::map<std::string, std::vector<double>> FeaturesProfileMap;
+
+struct FeaturesProfile {
+  int m_domain_modifier;
+  int m_ptm_modifier;
+  int m_motif_modifier;
+  std::map<std::string, double> m_motif_probabilities;
+  FeaturesProfileMap m_feature_profile_scores;
+
+  FeaturesProfile(const std::vector<std::string>& features,
+                  int domain_modifier, int ptm_modifier, int motif_modifier,
+                  std::map<std::string, double> motif_probabilities);
+  FeaturesProfileMap create_features_profile(
+      const fasta::SequenceList& sequences);
+  FeaturesProfileMap create_score_features_profile(
+      const fasta::SequenceList& sequences);
+  double score_ptm(unsigned long position, std::string feat_name);
+  double score_domain(unsigned long position, std::string feat_name);
+  double score_motif(unsigned long position, std::string feat_name);
+  double score_usr_feature(unsigned long position, std::string feat_name);
+};
 FeaturesProfileMap create_features_profile(
     const fasta::SequenceList& sequences, 
     const std::vector<std::string>& features);
@@ -30,61 +49,61 @@ double score_motif(FeaturesProfileMap& p, unsigned long position,
 double score_usr_feature(FeaturesProfileMap& p, size_t position,
                          std::string feat_name);
 
-class FeaturesProfile {
-public:
-  ///
-  /// constructor, creates empty profile
-  /// @param dom domain score
-  /// @param phosph PTM score
-  /// @param motif motif score
-  /// @param lcr LCR gap modifier
-  ///
-  FeaturesProfile(int dom, int phosph, int motif,
-                  std::map<std::string, double> probs);
-  ///
-  /// extracts the score for features on a particular position from profile
-  /// @param position position in the profile
-  /// @param features list of features assigned to the residue
-  /// @param add_score output score
-  ///
-  void get_score(unsigned int position, FeaturesList& features,
-                double& add_score);
-  ///
-  /// creates a profile matrix from the given alignment
-  ///
-  // TODO: How does this CreateProfile differ to that in 'profile.h'?
-  void CreateProfile(const std::vector<fasta::Sequence>& alignment,
-                     int codon_length);
-  void ProcessProfile();
-  ///
-  /// processes the matrix of feature occurences creating a matrix of scores
-  /// for aligning features at certain positions
-  ///
-  void CountOccurences(const std::vector<fasta::Sequence>& alignment, int codon_length);
-  ///
-  /// Takes a list of sequences, finds motifs and domains in it and adds them
-  /// to the list of features
-  ///
-  void ExpandListOfFeatures(const std::vector<fasta::Sequence> sequences);
-
-  /// Finds index of a certain feature in the profile
-  int FindFeaturesIndex(std::string& feat_name);
-private:
-  double GetMotifsProb(std::string& m_id);
-  double ScoreMotifs(unsigned int& position, std::string& feat_name);
-  double ScoreDomains(unsigned int& position, std::string& dom_name);
-  double ScorePTMs(unsigned int& position, std::string& ptm_name);
-  double ScoreUsrFeatures(unsigned int& position, std::string& feat_name);
-  ///
-  /// returns score modifier for a given feature
-  ///
-  double GetModifier(std::string& feat_name);
-  int m_domain_score, m_phosph_score, m_motif_score;
-  std::vector<std::string> m_motifs_ids;
-  std::vector<double> m_motifs_probs;
-  PrcRulesList m_rules;
-  ProfileMatrix m_prf_matrix;
-  ProfileMatrix m_occurences_matrix;
-};
+// class FeaturesProfile {
+// public:
+//   ///
+//   /// constructor, creates empty profile
+//   /// @param dom domain score
+//   /// @param phosph PTM score
+//   /// @param motif motif score
+//   /// @param lcr LCR gap modifier
+//   ///
+//   FeaturesProfile(int dom, int phosph, int motif,
+//                   std::map<std::string, double> probs);
+//   ///
+//   /// extracts the score for features on a particular position from profile
+//   /// @param position position in the profile
+//   /// @param features list of features assigned to the residue
+//   /// @param add_score output score
+//   ///
+//   void get_score(unsigned int position, FeaturesList& features,
+//                 double& add_score);
+//   ///
+//   /// creates a profile matrix from the given alignment
+//   ///
+//   // TODO: How does this CreateProfile differ to that in 'profile.h'?
+//   void CreateProfile(const std::vector<fasta::Sequence>& alignment,
+//                      int codon_length);
+//   void ProcessProfile();
+//   ///
+//   /// processes the matrix of feature occurences creating a matrix of scores
+//   /// for aligning features at certain positions
+//   ///
+//   void CountOccurences(const std::vector<fasta::Sequence>& alignment, int codon_length);
+//   ///
+//   /// Takes a list of sequences, finds motifs and domains in it and adds them
+//   /// to the list of features
+//   ///
+//   void ExpandListOfFeatures(const std::vector<fasta::Sequence> sequences);
+// 
+//   /// Finds index of a certain feature in the profile
+//   int FindFeaturesIndex(std::string& feat_name);
+// private:
+//   double GetMotifsProb(std::string& m_id);
+//   double ScoreMotifs(unsigned int& position, std::string& feat_name);
+//   double ScoreDomains(unsigned int& position, std::string& dom_name);
+//   double ScorePTMs(unsigned int& position, std::string& ptm_name);
+//   double ScoreUsrFeatures(unsigned int& position, std::string& feat_name);
+//   ///
+//   /// returns score modifier for a given feature
+//   ///
+//   double GetModifier(std::string& feat_name);
+//   int m_domain_score, m_phosph_score, m_motif_score;
+//   std::vector<std::string> m_motifs_ids;
+//   std::vector<double> m_motifs_probs;
+//   PrcRulesList m_rules;
+//   ProfileMatrix m_prf_matrix;
+//   ProfileMatrix m_occurences_matrix;
+// };
 
 #endif /* FEATURESPROFILE_H */
