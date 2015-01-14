83d2f9cfb4366d1e1332dc2527e342eddb55c07b
Joanna Lange <joasialange@gmail.com>
2015-01-14 12:33:24 +0100

    Changed performNWAlignment to backtrace_alignment_path

--- src/msa.cpp
+++ src/msa.cpp
@@ -158,8 +158,8 @@ fasta::SequenceList msa::align_pairwise(const fasta::Sequence& input_sequence,
   ScoringMatrix scores(profile_length, input_sequence.residues.size(),
                        gap_open_pen, end_pen, gap_ext_pen);
   scores.calculate_scores(input_sequence, profile, f_profile, codon_length);
-  //scores.PerformNWAlignment(alignment, seq2, profile, feat_prf,
-                            //codon_length);
+  scores.backtrace_alignment_path(alignment, input_sequence, profile, 
+                                  f_profile, codon_length);
   fasta::SequenceList aligned_sequence;
   aligned_sequence = remove_gaps(alignment);
   return aligned_sequence;
--- src/scoring_matrix.h
+++ src/scoring_matrix.h
@@ -39,9 +39,11 @@ public:
   ///
   /// traces back the alignment path in the scoring matrices
   ///
-  // void PerformNWAlignment(std::vector<fasta::Sequence>& result,
-  //                        fasta::Sequence s2, Profile& prf,
-  //                        FeaturesProfile& feat_prf, int codon_length);
+  void backtrace_alignment_path(fasta::SequenceList& result,
+                                const fasta::Sequence& sequence, 
+                                const ProfileMap& profile,
+                                const FeaturesProfile& f_profile,
+                                int codon_length);
 private:
   ///
   /// finds the best score either in the last column or in the last row of the
