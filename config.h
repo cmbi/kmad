c183a93bee0836ab25293730d04ce51c377005a7
Joanna Lange <joasialange@gmail.com>
2015-05-01 11:16:56 +0200

    add missing test files to Makefile.am

--- config.h
+++ config.h
@@ -101,7 +101,7 @@
 #define PACKAGE_NAME "kmad"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "kmad 0.0.0"
+#define PACKAGE_STRING "kmad 1.05"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "kmad"
@@ -110,13 +110,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.0"
+#define PACKAGE_VERSION "1.05"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.0.0"
+#define VERSION "1.05"
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
